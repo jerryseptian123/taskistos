3e3bd958814c152df21fcd6ffca313893068e341
jerryseptian123 <112109192+jerryseptian123@users.noreply.github.com>
2025-08-19 02:18:05 +0700

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,17 +2,19 @@
 # see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstabl
+  channel = "stable-24.05"; # or "unstable"
+
   # Use https://search.nixos.org/packages to find packages
   packages = [
-	  pkgs.python312
+    pkgs.python312
     pkgs.nodejs_20
   ];
+
   idx = {
     extensions = [ "ms-python.python" ];
-    workspace = {};
-	  previews = {
-	    enable = false;
-	  };
-};
+    workspace = { };
+    previews = {
+      enable = false;
+    };
+  };
 }
